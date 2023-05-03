d3d98587926991c204bcae8dd4733f7cb1984d38
eric antunes <antunesv.eric@gmail.com>
2023-05-03 11:43:06 -0300

    feat: finishing toast, refactoring and creating the event manager

--- src/pages/NewContact/index.jsx
+++ src/pages/NewContact/index.jsx
@@ -1,6 +1,7 @@
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function NewContact() {
   async function handleSubmit(formData) {
@@ -12,11 +13,17 @@ export default function NewContact() {
         category_id: formData.categoryId,
       };
 
-      const response = await ContactsService.createContact(contact);
+      await ContactsService.createContact(contact);
 
-      console.log(response);
+      toast({
+        type: 'sucess',
+        text: 'Contato cadastrado com sucesso',
+      });
     } catch {
-      alert('Ocorreu um erro ao cadastrar');
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao cadastrar o contato! ',
+      });
     }
   }
 
